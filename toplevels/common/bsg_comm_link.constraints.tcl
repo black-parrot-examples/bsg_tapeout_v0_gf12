15c222cc11c8bc055dc34d713479de367765b17d
hema0730 <zhaochun07@gmail.com>
2019-06-22 10:16:30 -0700

    set io constraints at inner side

--- toplevels/common/bsg_comm_link.constraints.tcl
+++ toplevels/common/bsg_comm_link.constraints.tcl
@@ -21,7 +21,7 @@ proc bsg_comm_link_timing_constraints { \
 
   set sdi_clk sdi_${ch_idx}_clk
   create_clock -period $io_clk_period -name $sdi_clk $ch_in_clk_port
-  set_clock_latency 600 [get_clocks $sdi_clk]
+  #set_clock_latency 600 [get_clocks $sdi_clk]
 
   set sdo_tkn_clk sdo_${ch_idx}_tkn_clk
   create_clock -period $tkn_clk_period -name $sdo_tkn_clk $ch_out_tkn_port
@@ -38,7 +38,7 @@ proc bsg_comm_link_timing_constraints { \
 if { $USE_GENERATED_CLOCK } {
   set sdo_clk sdo_${ch_idx}_clk
   create_generated_clock -edges {2 4 6} -master_clock $iom_clk_name -source [get_attribute [get_clocks $iom_clk_name] sources] -name $sdo_clk $ch_out_clk_port
-  set_clock_latency 1300 [get_clocks $sdo_clk]
+  #set_clock_latency 1300 [get_clocks $sdo_clk]
 
   set max_output_delay [expr ($io_clk_period / 4.0) - $max_io_skew_time]
   set min_output_delay [expr $max_io_skew_time - ($io_clk_period / 4.0)]
