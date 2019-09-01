bf22e2ab0e0cae4d4086be7f3ff89dbdd3a05037
Scott Davidson <scottdavidson1992@gmail.com>
2019-09-01 13:48:44 -0700

    [bsg_ac_black_parrot_sexta_core_rc0] much tighter floorplan. Updated pin constraints to use new bsg pins library constraints.

--- toplevels/bsg_ac_black_parrot_sexta_core_rc0/tcl/hard/gf_14/block_shaping.tcl
+++ toplevels/bsg_ac_black_parrot_sexta_core_rc0/tcl/hard/gf_14/block_shaping.tcl
@@ -7,9 +7,10 @@ set tile_rows 4
 set tile_cols 4
 
 set channel_width 5
-set channel_height 5
+set channel_height 35
 set tile_width [round_up_to_nearest [expr $core_width/4 - 10*$channel_width] [unit_width]]
-set tile_height [round_up_to_nearest [expr $core_height/4 - 10*$channel_height] [unit_height]]
+#set tile_height [round_up_to_nearest [expr $core_height/7 - 10*$channel_height] [unit_height]]
+set tile_height [round_up_to_nearest 320.000 [unit_height]]
 
 # Manual swizzle. There's a clever way to do this, but I'll leave it to leetcode
 set bp_tile_cells [list]
@@ -24,7 +25,7 @@ bsg_create_block_array_grid $bp_tile_cells \
   -y [expr $channel_height] \
   -rows $tile_rows \
   -cols $tile_cols \
-  -min_channel [list $channel_width 10*$channel_height] \
+  -min_channel [list $channel_width $channel_height] \
   -width $tile_width \
   -height $tile_height
 
